cade78d75b8dc977af42c332a29b55ed04245fba
Lupercar <hotolete@gmail.com>
2016-08-28 23:17:11 +0200

    Trabajamos a nivel de carácter con BufferedReader

--- src/consola/App.java
+++ src/consola/App.java
@@ -1,12 +1,17 @@
 package consola;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class App {
 
 	public static void main(String[] args) throws IOException {
+//		trabajar a nivel de bytes.
+//		==========================
+		
 //		FileInputStream fin = new FileInputStream("c:/Temp/demo.txt"); 
 //		BufferedInputStream bfin = new BufferedInputStream(fin);
 		
@@ -18,5 +23,16 @@ public class App {
 			System.out.println( (char)acabar); 
 		
 		bfin.close();
+		
+//		trabajar a nivel de carácter
+//		============================
+		BufferedReader bfr = new BufferedReader(
+				new FileReader("c:/Temp/demo.txt"));
+		
+		String linea = ""; 
+		while( (linea = bfr.readLine() ) != null)
+			System.out.println(linea);
+		
+		bfr.close();
 	}
 }//fin class consola.App
