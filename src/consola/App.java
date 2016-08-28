77a7414d7eb00b5e315b5e7b9ddf734b58b3bbc3
Lupercar <hotolete@gmail.com>
2016-08-28 19:08:45 +0200

    Simplifico BufferInputStream

--- src/consola/App.java
+++ src/consola/App.java
@@ -7,8 +7,11 @@ import java.io.IOException;
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		FileInputStream fin = new FileInputStream("c:/Temp/demo.txt"); 
-		BufferedInputStream bfin = new BufferedInputStream(fin);
+//		FileInputStream fin = new FileInputStream("c:/Temp/demo.txt"); 
+//		BufferedInputStream bfin = new BufferedInputStream(fin);
+		
+		BufferedInputStream bfin = new BufferedInputStream(
+				new FileInputStream("c:/Temp/demo.txt"));
 		
 		int acabar; 
 		while( (acabar = bfin.read()) != -1) 
