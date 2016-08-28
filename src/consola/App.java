a30456f681a5e35c359131361668b6d92eb8a98f
Lupercar <hotolete@gmail.com>
2016-08-28 19:05:47 +0200

    Crear un BufferedInputStream a partir de un FileInputStream

---
+++ src/consola/App.java
@@ -0,0 +1,19 @@
+package consola;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fin = new FileInputStream("c:/Temp/demo.txt"); 
+		BufferedInputStream bfin = new BufferedInputStream(fin);
+		
+		int acabar; 
+		while( (acabar = bfin.read()) != -1) 
+			System.out.println( (char)acabar); 
+		
+		bfin.close();
+	}
+}//fin class consola.App
